16a85602925711f5637fb0897edacaf4ade1cd95
Alex Bair <alexbair143@gmail.com>
2024-04-27 23:48:59 -0400

    fix(frontend-helpers): :bug: Export each helper function so they can be individually imported.

--- frontend/src/helpers.js
+++ frontend/src/helpers.js
@@ -1,11 +1,11 @@
-function removeBinFromPath(path) {
+export function removeBinFromPath(path) {
   const relevantPathArray = path.split('/');
   const relevantPath = '/' + relevantPathArray.slice(2).join('/');
 
   return relevantPath;
 }
 
-function determineBinId(path) {
+export function determineBinId(path) {
   return path.split('/')[1];
 }
 
